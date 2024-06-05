73733e3637728b637302cb408011494f4a89e68f
wdwdwq <qkenr7278@gmail.com>
2024-06-05 15:02:21 +0900

    각도기 switch

--- src/main/java/com/ki/level0/pl20240605/Main.java
+++ src/main/java/com/ki/level0/pl20240605/Main.java
@@ -15,18 +15,52 @@ public class Main {
 
 class Solution {
     public int solution(int angle) {
+        //for문
+//        int answer = 0;
+//        if(0 < angle && angle < 90) {
+//            answer = 1;
+//        } else if(angle == 90) {
+//            answer = 2;
+//        } else if(90 < angle && angle < 180) {
+//            answer = 3;
+//        } else {
+//            answer = 4;
+//        }
+//        return answer;
+//    }
+        //switch
         int answer = 0;
-        if(0 < angle && angle < 90) {
-            answer = 1;
-        } else if(angle == 90) {
-            answer = 2;
-        } else if(90 < angle && angle < 180) {
-            answer = 3;
-        } else {
-            answer = 4;
+        String gak = null;
+        if(0 < angle && angle < 90){
+            gak = "예각";
+        };
+        if(angle == 90){
+            gak = "직각";
+        };
+        if(90 < angle && angle < 180){
+            gak = "둔각";
+        };
+        if(angle == 180){
+            gak = "평각";
+        };
+
+
+        switch(gak){
+            case "예각":
+                answer = 1;
+                break;
+            case "직각":
+                answer = 2;
+                break;
+            case "둔각":
+                answer = 3;
+                break;
+            case "평각":
+                answer = 4;
+                break;
         }
         return answer;
-    }
+    };
 
 }
 
