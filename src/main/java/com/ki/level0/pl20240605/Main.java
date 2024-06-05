442201b858cba9d62797ccd38f2968f1d332254a
wdwdwq <qkenr7278@gmail.com>
2024-06-05 14:29:08 +0900

    배열 뒤집기

--- src/main/java/com/ki/level0/pl20240605/Main.java
+++ src/main/java/com/ki/level0/pl20240605/Main.java
@@ -1,18 +1,37 @@
 package com.ki.level0.pl20240605;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(2, 3)); // -1
-        System.out.println(new Solution().solution(11, 11)); // 1
-        System.out.println(new Solution().solution(7, 99)); // -1
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        int[] numbers2 = {1, 1, 1, 1, 1, 2};
+        int[] numbers3 = {1, 0, 1, 1, 1, 3, 5};
+
+        Solution solution = new Solution();
+        int [] result1 = solution.solution(numbers1);
+        int [] result2 = solution.solution(numbers2);
+        int [] result3 = solution.solution(numbers3);
+
+        System.out.println("numbers1의 뒤집은 배열: " + Arrays.toString(result1));
+        System.out.println("numbers2의 뒤집은 배열: " + Arrays.toString(result2));
+        System.out.println("numbers3의 뒤집은 배열: " + Arrays.toString(result3));
     }
 }
 
+
 class Solution {
-    public int solution(int num1, int num2){
-        if(num1 == num2){
-          return 1;
+    public int[] solution(int[] num_list) {
+        int length = num_list.length; // 입력한 배열의 길이를 구한다
+        int[] answer = new int[length]; // 동일한 길이의 새로운 배열을 생성
+
+        // 입력 배열의 요소를 뒤집어 새로운 배열에 저장
+        for(int i = 0; i < length; i++){
+            answer[i] = num_list[length - 1 - i]; // 마지막 요소부터 차례로
         }
-        return -1;
+
+        return answer; // 뒤집어진 배열을 반환함
     }
 }
+
+
--- src/test/java/com/ki/level0/pl20240605/Tests.java
+++ src/test/java/com/ki/level0/pl20240605/Tests.java
@@ -5,27 +5,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-//        System.out.println(new Solution().solution(2, 3)); // -1
-//        System.out.println(new Solution().solution(11, 11)); // 1
-//        System.out.println(new Solution().solution(7, 99)); // -1
-
 public class Tests {
 
     @Test
-    @DisplayName("2랑 3을 넣으면 -1이 나와야해")
+    @DisplayName("1,2,3,4,5 => 5,4,3,2,1 ")
     void t1() {
-        assertThat(new Solution().solution(2, 3)).isEqualTo(-1);
+        assertThat(new Solution().solution(new int[]{1,2,3,4,5})).isEqualTo(new int[]{5,4,3,2,1});
     }
 
     @Test
-    @DisplayName("11, 11 => 1")
+    @DisplayName("1, 1, 1, 1, 1, 2 => 2, 1, 1, 1, 1, 1 ")
     void t2() {
-        assertThat(new Solution().solution(11, 11)).isEqualTo(1);
+        assertThat(new Solution().solution(new int[]{1,1,1,1,1,2})).isEqualTo(new int[]{2, 1, 1, 1, 1, 1});
     }
 
     @Test
-    @DisplayName("7, 99 => -1")
+    @DisplayName("1, 0, 1, 1, 1, 3, 5 => 5, 3, 1, 1, 1, 0, 1 ")
     void t3() {
-        assertThat(new Solution().solution(7, 99)).isEqualTo(-1);
+        assertThat(new Solution().solution(new int[]{1, 0, 1, 1, 1, 3, 5})).isEqualTo(new int[]{5, 3, 1, 1, 1, 0, 1});
     }
 }
